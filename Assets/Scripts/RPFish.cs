acaffda40c06f038de4e9fe22e06707d049c7866
FineAp <143973266+FineAp@users.noreply.github.com>
2023-09-04 09:59:28 +0900

    Update and rename RPFish.cs to Assets/Scripts/RPFish.cs

--- Assets/Scripts/RPFish.cs
+++ Assets/Scripts/RPFish.cs
@@ -15,7 +15,7 @@ public class RPFish : MonoBehaviour
         if (curSpawnDelay > maxSpawnDelay)
         {
             SpawnEnemy();
-            maxSpawnDelay = Random.Range(0.5f, 1f); //·£´ı Ä«¿îÆ®
+            maxSpawnDelay = Random.Range(0.5f, 1f); //ëœë¤ ì¹´ìš´íŠ¸
             curSpawnDelay = 0;
         }
 
@@ -23,8 +23,8 @@ public class RPFish : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int ranEnemy = Random.Range(0, 3); //¼ÒÈ­µÉ ¿ÀºêÁ§Æ® ¼ıÀÚ
-        int ranPoint = Random.Range(0, 10); //·£´ı À§Ä¡ ¼ıÀÚ 5°÷
+        int ranEnemy = Random.Range(0, 3); //ì†Œí™”ë  ì˜¤ë¸Œì íŠ¸ ìˆ«ì
+        int ranPoint = Random.Range(0, 10); //ëœë¤ ìœ„ì¹˜ ìˆ«ì 5ê³³
         Instantiate(enemyObjs[ranEnemy],
                     spawnPoints[ranPoint].position,
                     spawnPoints[ranPoint].rotation);
