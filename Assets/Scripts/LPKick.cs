f683f80b368fa11914dd0196151a5132f3db7973
FineAp <143973266+FineAp@users.noreply.github.com>
2023-09-04 09:53:46 +0900

    Update and rename LPKick.cs to Assets/Scripts/LPKick.cs

--- Assets/Scripts/LPKick.cs
+++ Assets/Scripts/LPKick.cs
@@ -15,7 +15,7 @@ public class LPKick : MonoBehaviour
         if (curSpawnDelay > maxSpawnDelay)
         {
             SpawnEnemy();
-            maxSpawnDelay = Random.Range(0.5f, 1f); //·£´ı Ä«¿îÆ®
+            maxSpawnDelay = Random.Range(0.5f, 1f); //ëœë¤ ì¹´ìš´íŠ¸
             curSpawnDelay = 0;
         }
 
@@ -23,8 +23,8 @@ public class LPKick : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int ranEnemy = Random.Range(0, 4); //¼ÒÈ­µÉ ¿ÀºêÁ§Æ® ¼ıÀÚ
-        int ranPoint = Random.Range(0, 6); //·£´ı À§Ä¡ ¼ıÀÚ 5°÷
+        int ranEnemy = Random.Range(0, 4); //ì†Œí™”ë  ì˜¤ë¸Œì íŠ¸ ìˆ«ì
+        int ranPoint = Random.Range(0, 6); //ëœë¤ ìœ„ì¹˜ ìˆ«ì 5ê³³
         Instantiate(enemyObjs[ranEnemy],
                     spawnPoints[ranPoint].position,
                     spawnPoints[ranPoint].rotation);
